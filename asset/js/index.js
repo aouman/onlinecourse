75eb548541586f93a275d4cd8833c2fca63d9667
aouman <37551703+aouman@users.noreply.github.com>
2020-10-21 17:25:56 +0000

    le bouton login , l'icone de panier plus celui de recherche, dropdown search

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
   /*lorsqu'on clique sur l'un des bouton du menu elle s'active*/
-  $('.navbar-nav .nav-item a').on('click', function(e){
+  $('.navbar-nav .navs a').on('click', function(e){
     e.preventDefault();
     var me=$(this).parent().toggleClass('active')
     $(".active").not(me).removeClass('active')
   });
+
+  /*$(".nav-item .btn-search").click(function(){
+    $(this).css('display','none');
+    $(".nav-item form").css('display','block');
+  });*/
+
 
 });
